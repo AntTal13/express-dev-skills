9bf72f6ca1c769e88daec5762fd45a5b9bed9220
AntTal13 <anthonytalarico13@gmail.com>
2023-03-30 11:18:32 -0400

    skills update

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,17 +2,55 @@ const Skill = require('../models/skill');
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill,
+    edit,
+    update
 };
-  
+
+function update(req, res) {
+    req.body.done = !!req.body.done;
+    Skill.update(req.params.id, req.body);
+    res.redirect(`/skills/${req.params.id}`);
+}
+
+function edit(req, res) {
+    const skill = Skill.getOne(req.params.id);
+    res.render('skills/edit', {
+        title: 'Edit Skill',
+        skill
+    });
+}
+
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills');
+}
+
+function create(req, res) {
+    console.log(req.body);
+    // The model is responsible for creating data
+    Skill.create(req.body);
+    // Do a redirect anytime data is changed
+    res.redirect('/skills');
+}
+
+function newSkill(req, res) {
+    res.render('skills/new', { title: 'New Skill' });
+}
+
 function index(req, res) {
     res.render('skills/index', {
-        skills: Skill.getAll()
+        skills: Skill.getAll(),
+        title: 'All Skills'
     });
 }
 
 function show(req, res) {
     res.render('skills/show', {
       skill: Skill.getOne(req.params.id),
+      title: 'Skill Details'
     });
 }
--- models/skill.js
+++ models/skill.js
@@ -6,8 +6,33 @@ const skills = [
   
 module.exports = {
     getAll,
-    getOne
-};
+    getOne,
+    create,
+    deleteOne,
+    update
+  };
+  
+function update(id, updatedSkill) {
+    id = parseInt(id);
+    const skill = skills.find(skill => skill.id === id);
+    Object.assign(skill, updatedSkill);
+}
+
+function deleteOne(id) {
+    // All properties attached to req.params are strings!
+    id = parseInt(id);
+    // Find the index based on the id of the skill object
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+}
+
+function create(skill) {
+    // Add the id
+    skill.id = Date.now() % 1000000;
+    // New skills would be to learn
+    skill.done = false;
+    skills.push(skill);
+}
   
 function getAll() {
     return skills;
